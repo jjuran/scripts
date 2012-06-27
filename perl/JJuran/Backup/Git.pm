353dd412b5f1d1f8cd9c30c751c61ae51e55c124
Josh Juran <jjuran@metamage.com>
2012-06-26 21:34:27 -0700

    [NEW] Perl package JJuran::Backup::Git

---
+++ perl/JJuran/Backup/Git.pm
@@ -0,0 +1,51 @@
+package JJuran::Backup::Git;
+
+use warnings;
+use strict;
+
+
+my $var_git = "$ENV{HOME}/var/git";
+
+my $mirrors = $ENV{GIT_MIRROR_DIR};
+
+die "GIT_MIRROR_DIR not set\n"  if !defined $mirrors;
+
+die "Mirror directory $mirrors doesn't exist\n"  if !-d $mirrors;
+
+
+sub mirror_repo
+{
+	my ( $group, $name ) = @_;
+	
+	my $repo_dir = "$var_git/$group/$name";
+	
+	die "$repo_dir is not a Git repository\n"  if !-d "$repo_dir/.git";
+	
+	my $group_dir = "$mirrors/$group";
+	
+	my $mirror_dir = "$group_dir/$name.git";
+	
+	if ( !-d $mirror_dir )
+	{
+		mkdir $group_dir  if -d $group_dir;
+		
+		my @cmd = ( qw( git init --bare ), $mirror_dir );
+		
+		system( @cmd ) == 0 or die "Failed to git init $mirror_dir\n";
+	}
+	
+	my @cmd =
+	(
+		'git',
+		"--work-tree=$repo_dir",
+		"--git-dir=$repo_dir/.git",
+		'push',
+		'--mirror',
+		"$mirrors/$group/$name.git"
+	);
+	
+	system( @cmd ) == 0 or die "git push --mirror of $group/$name failed\n";
+}
+
+1;
+
